6eace679ae18693ce2e69b6de26d99bdfe87a125
tony_glo <tony_glo@tony.com>
2021-10-20 10:07:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+    <main-tab-bar/>
+  </div>
+</template>
+<script>
+import MainTabBar from 'components/content/tabbar/MainTabBar'
+export default {
+  name: 'app',
+  components: {
+    MainTabBar
+  }
+}
+</script>
+
+<style>
+</style>
